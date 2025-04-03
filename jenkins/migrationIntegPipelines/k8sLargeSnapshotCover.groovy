7758c924086cd4eb366532bc9391731b484dcd4e
Jugal Chauhan <jugaldc@amazon.com>
2025-04-03 12:22:38 -0500

    add groovy files
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

---
+++ jenkins/migrationIntegPipelines/k8sLargeSnapshotCover.groovy
@@ -0,0 +1,9 @@
+def gitBranch = params.GIT_BRANCH ?: 'test-k8s-large-snapshot'
+def gitUrl = params.GIT_REPO_URL ?: 'https://github.com/jugal-chauhan/opensearch-migrations.git'
+
+library identifier: "migrations-lib@${gitBranch}", retriever: modernSCM(
+        [$class: 'GitSCMSource',
+         remote: "${gitUrl}"])
+
+// Shared library function (location from root: vars/elasticsearch5xK8sLocalTest.groovy)
+k8sLargeSnapshotGenerator.groovy()
